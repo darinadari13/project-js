6a53d796a9712188495f08ede266d59c70dd1487
darinadari13 <darina460@gmail.com>
2023-01-20 20:18:54 +0200

    срфтпуі

---
+++ src/js/my-library.js
@@ -0,0 +1,11 @@
+const refs = {
+  addTowatchedBtn: document.querySelector('.film-modal-btn-action.accent'),
+  addToQueueBtn: document.querySelector('.film-modal-btn-action.transparent')
+}
+
+function onAddTowatchedBtnClick(e) {
+  e.preventDefault();
+}
+
+
+refs.addTowatchedBtn.addEventListener('click', onAddTowatchedBtnClick);
--- src/js/requestAPI.js
+++ src/js/requestAPI.js
@@ -3,36 +3,40 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
+
+
 const main = document.getElementById('main')
 getMovies(API_URL);
 
 
 function getMovies(url) {
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results);
-        showMovies(data.results);
-    })
+  fetch(url).then(res => res.json()).then(data => {
+    console.log(data.results);
+
+    showMovies(data.results);
+  })
 
 }
 
 function showMovies(data) {
-    main.innerHTML = '';
-    data.forEach(movie => {
-        const { title, poster_path, name, overview, id, release_date } = movie;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
+  main.innerHTML = '';
+  data.forEach(movie => {
+    const { title, poster_path, name, overview, id, release_date } = movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
              <img src="${IMG_URL + poster_path}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${(name)}">${name} | ${release_date.slice(0, 4)
-            } </span>  
+      } </span>  
          
         `
-        main.appendChild(movieEl);
-
-    })
+    main.appendChild(movieEl);
+  })
 }
+
+
 
 
 // const apiKey = '1deae1a36202e3ac8c29219a3d453e0f';
