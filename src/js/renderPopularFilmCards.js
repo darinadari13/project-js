d406cb4ba6e31a2bdbd37fd87c3adaf64c1863c8
Nadin Severin <nadinlikes2018@gmail.com>
2023-01-21 21:17:11 +0200

    hotFix

--- src/js/renderPopularFilmCards.js
+++ src/js/renderPopularFilmCards.js
@@ -1,5 +1,5 @@
 import debounce from 'lodash.debounce';
-import { TheMovieDbAPI } from './theMovieDbAPi';
+import { TheMovieDbAPI } from './theMovieDbAPI';
 
 const theMovieDbAPI = new TheMovieDbAPI();
 
