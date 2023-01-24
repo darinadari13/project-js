2065a5c881739b8004848186166d3b75df7ec531
snackli17 <snackli17@gmail.com>
2023-01-24 11:11:26 +0200

    photo
    photo

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -20,7 +20,7 @@ const markupTeam = `
 
             <div class="team-card">
                 <a href="https://github.com/Nadin-N" target="_blank" class="team-git">
-                     <img class="image_team" src="https://i.ibb.co/zbzM10k/photo-2022-09-28-00-42-22.jpg"   alt="Beautiful nature">
+                     <img class="image_team" src="https://i.ibb.co/zbzM10k/photo-2022-09-28-00-42-22.jpg"   alt="Nadin">
                 </a>
                 <p class="team-name">Nadin</p>
                 <p class="team-role">Scrum Master</p></div>
@@ -29,7 +29,7 @@ const markupTeam = `
 
             <div class="team-card">
                 <a href="https://github.com/ChernyshenkoI" target="_blank" class="team-git">
-                     <img class="image_team" src="https://placeimg.com/400/200/nature"   alt="Beautiful nature">
+                    <img src="https://i.ibb.co/PhKd08m/photo-2023-01-24-08-30-05.jpg"  alt="Iryna">
                 </a>
                 <p class="team-name">Iryna</p>
                 <p class="team-role">Developer</p></div>
@@ -37,7 +37,7 @@ const markupTeam = `
 
             <div class="team-card">
                 <a href="https://github.com/JulkaUlka" target="_blank" class="team-git">
-                     <img class="image_team" src="https://i.ibb.co/mG2LQtf/photo-of-me.jpg"   alt="Beautiful nature">
+                     <img class="image_team" src="https://i.ibb.co/mG2LQtf/photo-of-me.jpg"   alt="Yuliia">
                 </a>
                 <p class="team-name">Yuliia</p>
                 <p class="team-role">Developer</p></div>
@@ -46,7 +46,7 @@ const markupTeam = `
 
             <div class="team-card">
                 <a href="https://github.com/snackli17" target="_blank" class="team-git">
-                      <img class="image_team" src="https://i.ibb.co/cyhZXS5/image.png"   alt="Beautiful nature">
+                      <img src="https://i.ibb.co/K62SgmX/image.png"   alt="NATALIA">
                 </a>
                 <p class="team-name">NATALIA</p>
                 <p class="team-role">Developer</p></div>
