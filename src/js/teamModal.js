066059bbb20b9b88258cd29eaff1cbbf8936aeed
Yuliia Hlushko <y.glushko@outlook.com>
2023-01-25 22:24:19 +0200

    fixed team modal

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -1,8 +1,9 @@
 import * as basicLightbox from 'basiclightbox';
-import sprite from '../../images/icons.svg';
+
 
 const markupTeam = `
 		<div class="modal-team">
+        
             <div class="team-card">
                 <a href="https://github.com/darinadari13" target="_blank" class="team-git">
                     <img class="image_team" src="https://i.ibb.co/3BTGp6S/photo-2023-01-23-17-15-42.jpg"   alt="Dari">
@@ -43,7 +44,7 @@ const markupTeam = `
                 <a href="https://github.com/snackli17" target="_blank" class="team-git">
                       <img class="image_team" src="https://i.ibb.co/K62SgmX/image.png"   alt="NATALIA">
                 </a>
-                <p class="team-name">NATALIA</p>
+                <p class="team-name">Natalia</p>
                 <p class="team-role">Developer</p></div>
 
 
@@ -54,10 +55,13 @@ const markupTeam = `
                 </a>
                 <p class="team-name">Yuliia</p>
                 <p class="team-role">Developer</p></div>
-            <p class="title-team"> &#128153;  &#128155;    BAVOVNA    <confetti-button></confetti-button>     &#128105;&#8205;&#128187;</p>
-        </div>      
+            <div class="title-team"> <confetti-button></confetti-button>    </div>
+        </div>    
+            
       
 `;
+
+
 const linkFooter = document.querySelector('.js-team-modal');
 linkFooter.addEventListener('click', openModalTeam);
 const modalTeam = basicLightbox.create(markupTeam);
